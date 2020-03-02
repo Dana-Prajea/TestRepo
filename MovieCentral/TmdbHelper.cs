60038c1c1f9b9c843a930a545455bd44f30eedb8
Dana <dana@tunnelbear.com>
2020-03-02 11:46:16 -0500

    Added code

---
+++ MovieCentral/TmdbHelper.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MovieCentral
+{
+    public class Images
+    {
+        public string base_url { get; set; }
+        public string secure_base_url { get; set; }
+        public List<string> backdrop_sizes { get; set; }
+        public List<string> logo_sizes { get; set; }
+        public List<string> poster_sizes { get; set; }
+        public List<string> profile_sizes { get; set; }
+        public List<string> still_sizes { get; set; }
+    }
+
+    public class TmdbMain
+    {
+        public Images images { get; set; }
+        public List<string> change_keys { get; set; }
+    }
+
+    public class Result
+    {
+        public bool adult { get; set; }
+        public string backdrop_path { get; set; }
+        public int id { get; set; }
+        public string original_title { get; set; }
+        public string release_date { get; set; }
+        public string poster_path { get; set; }
+        public double popularity { get; set; }
+        public string title { get; set; }
+        public bool video { get; set; }
+        public double vote_average { get; set; }
+        public int vote_count { get; set; }
+    }
+
+    public class TmdbSearch
+    {
+        public int page { get; set; }
+        public List<Result> results { get; set; }
+        public int total_pages { get; set; }
+        public int total_results { get; set; }
+    }
+
+    public class Genre
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class ProductionCompany
+    {
+        public string name { get; set; }
+        public int id { get; set; }
+    }
+
+    public class ProductionCountry
+    {
+        public string iso_3166_1 { get; set; }
+        public string name { get; set; }
+    }
+
+    public class SpokenLanguage
+    {
+        public string iso_639_1 { get; set; }
+        public string name { get; set; }
+    }
+
+    public class MovieDetails
+    {
+        public bool adult { get; set; }
+        public string backdrop_path { get; set; }
+        public object belongs_to_collection { get; set; }
+        public int budget { get; set; }
+        public List<Genre> genres { get; set; }
+        public string homepage { get; set; }
+        public int id { get; set; }
+        public string imdb_id { get; set; }
+        public string original_language { get; set; }
+        public string original_title { get; set; }
+        public string overview { get; set; }
+        public double popularity { get; set; }
+        public string poster_path { get; set; }
+        public List<ProductionCompany> production_companies { get; set; }
+        public List<ProductionCountry> production_countries { get; set; }
+        public string release_date { get; set; }
+        public int revenue { get; set; }
+        public int runtime { get; set; }
+        public List<SpokenLanguage> spoken_languages { get; set; }
+        public string status { get; set; }
+        public string tagline { get; set; }
+        public string title { get; set; }
+        public bool video { get; set; }
+        public double vote_average { get; set; }
+        public int vote_count { get; set; }
+    }
+
+    public class Cast
+    {
+        public int cast_id { get; set; }
+        public string character { get; set; }
+        public string credit_id { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public int order { get; set; }
+        public string profile_path { get; set; }
+    }
+
+    public class Crew
+    {
+        public string credit_id { get; set; }
+        public string department { get; set; }
+        public int id { get; set; }
+        public string job { get; set; }
+        public string name { get; set; }
+        public string profile_path { get; set; }
+    }
+
+    public class MovieCredits
+    {
+        public int id { get; set; }
+        public List<Cast> cast { get; set; }
+        public List<Crew> crew { get; set; }
+    }
+}
